731c91e370a41289e4301fd40735d6427b9c4303
Matthew Sitton <matthewsitton@gmail.com>
2022-05-05 14:36:24 -0500

    Updated SocketWrapper
    SocketWrapper now has both async and sync methods

--- src/Fleck/SocketWrapper.cs
+++ src/Fleck/SocketWrapper.cs
@@ -11,114 +11,126 @@ using Fleck.Helpers;
 
 namespace Fleck
 {
-    public class SocketWrapper : ISocket
+    public class SslSocket
     {
-        public const uint KeepAliveInterval = 60000;
-        public const uint RetryInterval = 10000;
+        private readonly Socket socket;
+        private readonly CancellationTokenSource tokenSource;
+        private readonly SslServerAuthenticationOptions authenticationOptions;
+        private Stream stream;
 
-        private static readonly byte[] keepAliveValues;
-    
-        private readonly Socket _socket;
-        private CancellationTokenSource _tokenSource;
-        private TaskFactory _taskFactory;
+        public EndPoint LocalEndPoint => socket.LocalEndPoint;
+        public EndPoint RemoteEndPoint => socket.RemoteEndPoint;
 
-        public IPAddress RemoteIpAddress
+        public IPAddress? RemoteIpAddress => socket.GetRemoteIPAddress();
+        public int? RemotePort => socket.GetRemotePort();
+        public IPAddress? LocalIpAddress => socket.GetLocalIPAddress();
+        public int? LocalPort => socket.GetRemotePort();
+
+        public bool Connected => socket.Connected;
+        public int BytesAvailable => socket.Available;
+
+        public bool NoDelay
         {
-            get
-            {
-                var endpoint = _socket.RemoteEndPoint as IPEndPoint;
-                return endpoint?.Address;
-            }
+            get => socket.NoDelay;
+            set => socket.NoDelay = value;
         }
 
-        public int RemotePort => _socket.RemoteEndPoint is IPEndPoint endpoint ? endpoint.Port : -1;
-
-        static SocketWrapper()
+        public SslSocket(Socket socket)
         {
-            const int size = sizeof(uint);
-            uint on = 1;
+            if (socket.Connected)
+                throw new ConnectionNotAvailableException("Socket connection not available");
 
-            byte[] inArray = new byte[size * 3];
-            Array.Copy(BitConverter.GetBytes(on), 0, inArray, 0, size);
-            Array.Copy(BitConverter.GetBytes(KeepAliveInterval), 0, inArray, size, size);
-            Array.Copy(BitConverter.GetBytes(RetryInterval), 0, inArray, size * 2, size);
+            this.socket = socket;
+            tokenSource = new CancellationTokenSource();
 
-            keepAliveValues = inArray;
-        }
+            stream = new NetworkStream(this.socket);
+            authenticationOptions = new SslServerAuthenticationOptions();
 
-        public SocketWrapper(Socket socket)
-        {
-            _tokenSource = new CancellationTokenSource();
-            _taskFactory = new TaskFactory(_tokenSource.Token);
-            _socket = socket;
-            if (_socket.Connected)
-                Stream = new NetworkStream(_socket);
+            authenticationOptions.CertificateRevocationCheckMode = X509RevocationMode.NoCheck;
+            authenticationOptions.ClientCertificateRequired = false;
 
             // The tcp keepalive default values on most systems
             // are huge (~7200s). Set them to something more reasonable.
-            if (FleckRuntime.IsRunningOnWindows())
-                socket.IOControl(IOControlCode.KeepAliveValues, keepAliveValues, null);
+            socket.SetKeepAlive();
         }
 
-        public Task Authenticate(X509Certificate2 certificate, SslProtocols enabledSslProtocols, Action callback, Action<Exception> error)
+        public void Authenticate(X509Certificate2 certificate, SslProtocols enabledSslProtocols)
         {
-            var ssl = new SslStream(Stream, false);
-            Stream = new QueuedStream(ssl);
-            Func<AsyncCallback, object, IAsyncResult> begin =
-                (cb, s) => ssl.BeginAuthenticateAsServer(certificate, false, enabledSslProtocols, false, cb, s);
+            // Wrap network stream with ssl
+            var ssl = new SslStream(stream, false);
+            stream = ssl;
 
-            Task task = Task.Factory.FromAsync(begin, ssl.EndAuthenticateAsServer, null);
-            task.ContinueWith(t => callback(), TaskContinuationOptions.NotOnFaulted)
-                .ContinueWith(t => error(t.Exception), TaskContinuationOptions.OnlyOnFaulted);
-            task.ContinueWith(t => error(t.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            ssl.AuthenticateAsServer(certificate, false, enabledSslProtocols, false);
+        }
 
-            return task;
+        public Task AuthenticateAsync(X509Certificate2 certificate, SslProtocols enabledSslProtocols)
+        {
+            // Wrap network stream with ssl
+            var ssl = new SslStream(stream, false);
+            stream = ssl;
+
+            authenticationOptions.ServerCertificate = certificate;
+            authenticationOptions.EnabledSslProtocols = enabledSslProtocols;
+
+            return ssl.AuthenticateAsServerAsync(authenticationOptions, tokenSource.Token);
         }
 
         public void Listen(int backlog)
         {
-            _socket.Listen(backlog);
+            socket.Listen(backlog);
         }
 
         public void Bind(EndPoint endPoint)
         {
-            _socket.Bind(endPoint);
+            socket.Bind(endPoint);
         }
 
-        public bool Connected => _socket.Connected;
-
-        public Stream Stream { get; private set; }
-
-        public bool NoDelay
+        public int Read(Span<byte> data)
         {
-            get => _socket.NoDelay;
-            set => _socket.NoDelay = value;
+            return stream.Read(data);
         }
 
-        public EndPoint LocalEndPoint => _socket.LocalEndPoint;
-
-        public Task<ISocket> Accept(Action<ISocket> callback, Action<Exception> error)
+        public ValueTask<int> ReadAsync(Memory<byte> data)
         {
-            Func<IAsyncResult, ISocket> end = r => _tokenSource.Token.IsCancellationRequested ? null : new SocketWrapper(_socket.EndAccept(r));
-            var task = _taskFactory.FromAsync(_socket.BeginAccept, end, null);
-            task.ContinueWith(t => callback(t.Result), TaskContinuationOptions.OnlyOnRanToCompletion)
-                .ContinueWith(t => error(t.Exception), TaskContinuationOptions.OnlyOnFaulted);
-            task.ContinueWith(t => error(t.Exception), TaskContinuationOptions.OnlyOnFaulted);
-            return task;
+            return stream.ReadAsync(data, tokenSource.Token);
+        }
+
+        public void Write(ReadOnlySpan<byte> data)
+        {
+            stream.Write(data);
+        }
+
+        public ValueTask WriteAsync(ReadOnlyMemory<byte> data)
+        {
+            return stream.WriteAsync(data, tokenSource.Token);
+        }
+
+        public SslSocket Accept()
+        {
+            Socket clientSocket = socket.Accept();
+            var sockWrap = new SslSocket(clientSocket);
+            return sockWrap;
+        }
+
+        public async ValueTask<SslSocket> AcceptAsync()
+        {
+            Socket clientSocket = await socket.AcceptAsync();
+            var sockWrap = new SslSocket(clientSocket);
+            return sockWrap;
         }
 
         public void Dispose()
         {
-            _tokenSource.Cancel();
-            if (Stream != null) Stream.Dispose();
-            if (_socket != null) _socket.Dispose();
+            tokenSource.Cancel();
+            if (stream != null) stream.Dispose();
+            if (socket != null) socket.Dispose();
         }
 
         public void Close()
         {
-            _tokenSource.Cancel();
-            if (Stream != null) Stream.Close();
-            if (_socket != null) _socket.Close();
+            tokenSource.Cancel();
+            if (stream != null) stream.Close();
+            if (socket != null) socket.Close();
         }
     }
 }
