e084a4085c97d256ea6d41e9dc38e833809c80dc
Matthew Sitton <matthewsitton@gmail.com>
2022-05-28 15:55:48 -0500

    Add IHttpServiceHandler xml doc string

--- src/SpiderStud/Http/IHttpServiceHandler.cs
+++ src/SpiderStud/Http/IHttpServiceHandler.cs
@@ -7,6 +7,12 @@ namespace SpiderStud.Http
 {
     public interface IHttpServiceHandler : IDisposable
     {
+        /// <summary>
+        /// When an http request is recieved
+        /// </summary>
+        /// <param name="request">http headers</param>
+        /// <param name="connection">Active http connection, used to read request or send response data</param>
+        /// <returns>If the http connection should be kept alive after this request has been handled</returns>
         bool OnRequest(HttpRequest request, HttpConnection connection);
     }
 }
